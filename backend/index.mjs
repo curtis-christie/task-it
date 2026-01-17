78dbe5ede154364fada2ac610e0628ca370ed46d
Curtis Christie <103606492+curtis-christie@users.noreply.github.com>
2026-01-17 14:05:26 -0500

    DB connection, server listen

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,11 +1,23 @@
 //task-it/backend/index.mjs
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
+dotenv.config();
 
-const PORT = dotenv.config.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
-});
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected Successfully");
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to the database.");
+    process.exit(1);
+  });
