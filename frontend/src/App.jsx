9a71825527e02b7a8d31a7635ba9a7796ebc0cc6
Curtis Christie <103606492+curtis-christie@users.noreply.github.com>
2026-01-26 15:00:42 -0500

    add basic task add with button and input

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,33 @@
 //task-it/frontend/src/App.jsx
 import { useState } from "react";
+import mockTasks from "./utils/mockTasks.js";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(mockTasks);
+  const [task, setTask] = useState("");
   return (
     <>
-      <div>{tasks}!</div>
+      <h1>All Tasks</h1>
+      <label htmlFor="addTask">Add Task</label>
+      <br />
+      <input
+        aria-label="add new task"
+        type="text"
+        name="addTask"
+        id="addTask"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button>Add</button>
+      <div>
+        {tasks.map((task) => (
+          <p key={task.id}>{task.title}</p>
+        ))}
+      </div>
     </>
   );
 }
 
 export default App;
 
-//todo - header, input field with add button, task list below with checkbox and delete button
+//todo - add button click logic to update array of tasks, add delete button to task item
